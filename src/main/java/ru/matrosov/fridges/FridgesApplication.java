a93e04bed8eb3b3a5e6306fb78d874ebfeebfba1
Danil <matrosovdan@mail.ru>
2023-12-01 14:43:31 +0300

    Initial commit

---
+++ src/main/java/ru/matrosov/fridges/FridgesApplication.java
@@ -0,0 +1,13 @@
+package ru.matrosov.fridges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/matrosov/fridges/FridgesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matrosov.fridges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
